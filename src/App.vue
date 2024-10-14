ed2f1f8b26a5925e43c254e9fb1c23f50669438b
toni3q <tonyhper@gmail.com>
2024-10-14 16:33:24 +0200

    working navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,8 @@
 <script setup>
-  function showNavbarMenu(){
-    document.getElementById('navbarMenu').classList.toggle('hidden')
-    document.getElementById('navbarMenuIcon').classList.toggle('rotate-[-90deg]')
-  }
+  import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-
-  <div id="navbar" class="text-black flex justify-center mx-auto w-max">
-    <div class="fixed w-full py-[1.5vh] px-[1vh] text-black bg-gradient-to-r from-zinc-100/25 to-zinc-200/25 backdrop-blur">
-      
-      <!-- Click to start showNavbarMenu() in order to show the hidden menu. -->
-      <button id="showMenuButton" v-on:click="showNavbarMenu" class="mx-[1vh]"><i id="navbarMenuIcon" class="rotate-[-90deg] duration-300 text-[3vh] fa-solid fa-chevron-down"></i></button>
-      
-      <!-- This is the actual content of the hidden menu. -->
-      <div id="navbarMenu" class="ml-[2vh] mt-[1.5vh] py-[0.5vh] hidden">
-        <div class="mx-[1vh] my-[1vh]"><RouterLink to="" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Homepage</RouterLink></div>
-        <div class="mx-[1vh] my-[1vh]"><RouterLink to="" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Services</RouterLink></div>
-        <div class="mx-[1vh] my-[1vh]"><RouterLink to="" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Informations</RouterLink></div>
-      </div>
-      
-    </div>
-  </div>  
-
+  <Navbar />
   <RouterView />
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<script setup>
+  function showNavbarMenu(){
+    document.getElementById('navbarMenu').classList.toggle('hidden')
+    document.getElementById('navbarMenuIcon').classList.toggle('rotate-[-90deg]')
+  }
+</script>
+
+<template>
+  <div id="navbar" class="text-black flex justify-center mx-auto w-max">
+    <div class="fixed w-full py-[1.5vh] px-[1vh] text-black bg-gradient-to-r from-zinc-100/25 to-zinc-200/25 drop-shadow-2xl backdrop-blur">
+
+      <!-- Click #showMenuButton to start showNavbarMenu() in order to show the hidden menu. -->
+      <div class="flex">
+        <!-- 
+          Large screen => display: none; 
+          Mobile/iPad screen => display: block;
+        -->
+        <button id="showMenuButton" v-on:click="showNavbarMenu" class="mx-[1vh] lg:hidden block"><i id="navbarMenuIcon" class="rotate-[-90deg] duration-300 text-[3vh] fa-solid fa-chevron-down"></i></button>
+        <!-- 
+          Large screen => display: block; 
+          Mobile/iPad screen => display: none;
+        -->
+        <div id="" class="flex hidden lg:block">
+          <RouterLink to="homepage" class="drop-shadow-lg mx-[1vh] text-[2.5vh] hover:text-sky-500 duration-300">Homepage</RouterLink>
+          <RouterLink to="services" class="drop-shadow-lg mx-[1vh] text-[2.5vh] hover:text-sky-500 duration-300">Services</RouterLink>
+          <RouterLink to="informations" class="drop-shadow-lg mx-[1vh] text-[2.5vh] hover:text-sky-500 duration-300">Informations</RouterLink>
+        </div>
+      </div>
+
+      <!-- This is the actual content of the hidden menu. -->
+      <!-- 
+        Large screen => display: none;
+        Mobile/iPad screen => display: none; 
+        (because you need to click the button)
+      -->
+      <div id="navbarMenu" class="ml-[2vh] mt-[1.5vh] py-[0.5vh] lg:hidden hidden">
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="homepage" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Homepage</RouterLink></div>
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="services" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Services</RouterLink></div>
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="informations" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Informations</RouterLink></div>
+      </div>
+
+    </div>
+  </div>
+</template>
