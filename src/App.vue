ac449f8f1619981c3c2fe405ecb71e01143bc7d7
toni3q <tonyhper@gmail.com>
2024-10-14 07:11:54 +0200

    dropdown menu on navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,27 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  function showNavbarMenu(){
+    document.getElementById('navbarMenu').classList.toggle('hidden')
+    document.getElementById('navbarMenuIcon').classList.toggle('rotate-[-90deg]')
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="navbar" class="text-black flex justify-center mx-auto w-max">
+    <div class="fixed w-full py-[1.5vh] px-[1vh] text-black bg-gradient-to-r from-zinc-100/25 to-zinc-200/25 backdrop-blur">
+      
+      <!-- Click to start showNavbarMenu() in order to show the hidden menu. -->
+      <button id="showMenuButton" v-on:click="showNavbarMenu" class="mx-[1vh]"><i id="navbarMenuIcon" class="rotate-[-90deg] duration-300 text-[3vh] fa-solid fa-chevron-down"></i></button>
+      
+      <!-- This is the actual content of the hidden menu. -->
+      <div id="navbarMenu" class="ml-[2vh] mt-[1.5vh] py-[0.5vh] hidden">
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Homepage</RouterLink></div>
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Services</RouterLink></div>
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="" class="drop-shadow-lg text-[2.5vh] hover:text-sky-500 duration-300">Informations</RouterLink></div>
+      </div>
+      
     </div>
-  </header>
+  </div>  
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
