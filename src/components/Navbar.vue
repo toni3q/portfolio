a868b6dee8b3c6ef8037c1003e1f10b0c9291ec6
toni3q <tonyhper@gmail.com>
2024-10-18 00:20:51 +0200

    portfolio informations update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,12 +7,12 @@
 
 <template>
   <div id="nav-container" class="h-[6vh] text-black flex justify-center mx-auto w-max">
-    <div id="navbar" class="z-50 fixed w-screen py-[1.5vh] px-[1vh] text-black bg-gradient-to-r from-white/80 to-white/80 drop-shadow-2xl backdrop-blur-lg">
-      <button id="showMenuButton" v-on:click="showNavbarMenu" class="mx-[1vh]"><i id="navbarMenuIcon" class="rotate-[-90deg] duration-300 text-[2vh] fa-solid fa-chevron-down"></i></button>
+    <div id="navbar" class="z-50 fixed w-screen py-[1.2vh] px-[1vh] text-black bg-gradient-to-t from-zinc-100/80 to-white/80 drop-shadow-xl backdrop-blur-lg">
+      <button id="showMenuButton" v-on:click="showNavbarMenu" class="mx-[1vh]"><i id="navbarMenuIcon" class="rotate-[-90deg] duration-500 text-[2vh] fa-solid fa-chevron-down"></i></button>
       <div id="navbarMenu" class="ml-[2vh] mt-[1.5vh] py-[0.5vh] lg:hidden hidden">
-        <div class="mx-[1vh] my-[1vh]"><RouterLink to="/" class="drop-shadow-sm hover:drop-shadow-md text-[2.5vh] text-zinc-600 hover:text-zinc-950 duration-300">Homepage</RouterLink></div>
-        <div class="mx-[1vh] my-[1vh]"><RouterLink to="/services" class="drop-shadow-sm hover:drop-shadow-md text-[2.5vh] text-zinc-600 hover:text-zinc-950 duration-300">Services</RouterLink></div>
-        <div class="mx-[1vh] my-[1vh]"><RouterLink to="/informations" class="drop-shadow-sm hover:drop-shadow-md text-[2.5vh] text-zinc-600 hover:text-zinc-950 duration-300">Informations</RouterLink></div>
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="/" class="dm-sans drop-shadow-sm hover:drop-shadow-md text-[2.5vh] text-zinc-500 hover:text-zinc-950 duration-300">Introduction</RouterLink></div>
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="/services" class="dm-sans drop-shadow-sm hover:drop-shadow-md text-[2.5vh] text-zinc-500 hover:text-zinc-950 duration-300">Projects</RouterLink></div>
+        <div class="mx-[1vh] my-[1vh]"><RouterLink to="/informations" class="dm-sans drop-shadow-sm hover:drop-shadow-md text-[2.5vh] text-zinc-500 hover:text-zinc-950 duration-300">Informations</RouterLink></div>
       </div>
     </div>
   </div>
